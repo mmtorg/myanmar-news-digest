0e60cc9b63b310c1e1e9ad0cf3c0f8c495388746
y_kawashima <86140172+gurayasu@users.noreply.github.com>
2025-07-31 15:19:33 +0630

    Update fetch_articles.py

--- fetch_articles.py
+++ fetch_articles.py
@@ -275,9 +275,12 @@ def process_and_summarize_articles(articles, source_name):
     return results
 
 def send_email_digest(summaries, subject="Daily Myanmar News Digest"):
-    sender_email = os.getenv("EMAIL_SENDER")
-    sender_pass = os.getenv("GMAIL_APP_PASSWORD")
-    recipient_emails = os.getenv("EMAIL_RECIPIENTS", "").split(",")
+    sender_email = "yasu.23721740311@gmail.com"
+    sender_pass = "mwdr ewpr ncfk vuuw"
+    recipient_emails = ["yasu.23721740311@gmail.com", "yasu.23721740311+1@gmail.com"]
+    # sender_email = os.getenv("EMAIL_SENDER")
+    # sender_pass = os.getenv("GMAIL_APP_PASSWORD")
+    # recipient_emails = os.getenv("EMAIL_RECIPIENTS", "").split(",")
 
     # メール本文のHTML生成
     html_content = "<html><body>"
