70de0e5f07bd9686b0bba328d81389597020c23e
kawashima <yasu.23721740311@gmail.com>
2025-08-15 12:13:07 +0630

    テスト用準備適応

--- fetch_articles.py
+++ fetch_articles.py
@@ -22,7 +22,10 @@ import pprint
 # 記事重複排除ロジック(BERT埋め込み版)のライブラリインポート
 
 # Gemini本番用
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+# client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+# GeminiTEST用
+client = genai.Client(api_key=os.getenv("GEMINI_TEST_API_KEY"))
 
 # Chat GPT
 # client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -35,7 +38,9 @@ MMT = timezone(timedelta(hours=6, minutes=30))
 # ニュースの速報性重視で今日分のニュース配信の方針
 def get_today_date_mmt():
     # 本番用、今日の日付
-    now_mmt = datetime.now(MMT)
+    # now_mmt = datetime.now(MMT)
+    # テスト用、昨日の日付にする
+    now_mmt = datetime.now(MMT) - timedelta(days=1)
     return now_mmt.date()
 
 
@@ -1193,7 +1198,9 @@ def send_email_digest(summaries):
     sender_email = os.getenv("EMAIL_SENDER")
     sender_pass = os.getenv("GMAIL_APP_PASSWORD")
     # メール送信先本番用
-    recipient_emails = os.getenv("EMAIL_RECIPIENTS", "").split(",")
+    # recipient_emails = os.getenv("EMAIL_RECIPIENTS", "").split(",")
+    # メール送信先テスト用
+    recipient_emails = ["yasu.23721740311@gmail.com"]
 
     # ✅ 今日の日付を取得してフォーマット
     digest_date = get_today_date_mmt()
