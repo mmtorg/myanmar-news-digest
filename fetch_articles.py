fcad29638034eae0bdbb2308bb13baecc51b8c3b
kawashima <yasu.23721740311@gmail.com>
2025-08-17 10:07:56 +0630

    Revert "テスト用準備反映"
    This reverts commit f57365a098cc87f1450f77eed5f3d7e0137b20c3.

--- fetch_articles.py
+++ fetch_articles.py
@@ -45,11 +45,6 @@ client_summary = genai.Client(api_key=os.getenv("GEMINI_API_SUMMARY_KEY"))
 client_dedupe = genai.Client(api_key=os.getenv("GEMINI_API_DEDUPE_KEY"))
 
 
-# Geminiテスト用
-client_summary = genai.Client(api_key=os.getenv("GEMINI_API_TEST_SUMMARY_KEY"))
-client_dedupe = genai.Client(api_key=os.getenv("GEMINI_API_TEST_DEDUPE_KEY"))
-
-
 # Chat GPT
 # client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
@@ -135,9 +130,7 @@ MMT = timezone(timedelta(hours=6, minutes=30))
 # ニュースの速報性重視で今日分のニュース配信の方針
 def get_today_date_mmt():
     # 本番用、今日の日付
-    # now_mmt = datetime.now(MMT)
-    # テスト用、昨日の日付にする
-    now_mmt = datetime.now(MMT) - timedelta(days=1)
+    now_mmt = datetime.now(MMT)
     return now_mmt.date()
 
 
@@ -1706,9 +1699,7 @@ def send_email_digest(summaries):
     sender_email = os.getenv("EMAIL_SENDER")
     sender_pass = os.getenv("GMAIL_APP_PASSWORD")
     # メール送信先本番用
-    # recipient_emails = os.getenv("EMAIL_RECIPIENTS", "").split(",")
-    # メール送信先テスト用
-    recipient_emails = ["yasu.23721740311@gmail.com"]
+    recipient_emails = os.getenv("EMAIL_RECIPIENTS", "").split(",")
 
     # ✅ 今日の日付を取得してフォーマット
     digest_date = get_today_date_mmt()
