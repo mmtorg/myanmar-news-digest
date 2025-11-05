88272fb8647a89f82936f6940b6ab5bbe34d5a62
y_kawashima <86140172+gurayasu@users.noreply.github.com>
2025-11-05 15:44:24 +0630

    テスト用記事取得範囲修正

--- sheet_pipeline.py
+++ sheet_pipeline.py
@@ -634,7 +634,7 @@ def _collect_all_for(target_date_mmt: date) -> List[Dict]:
         # (collect_irrawaddy_all_for_date, {}),
         (collect_khitthit_all_for_date, {"max_pages": 1}),
         # (collect_dvb_all_for_date, {}),
-        # (collect_myanmar_now_mm_all_for_date, {"max_pages": 3}),
+        (collect_myanmar_now_mm_all_for_date, {"max_pages": 1}),
     ]:
         name = fn.__name__.replace("_all_for_date", "")
         try:
